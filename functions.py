68c2066b76893b3a74f3d320ddfb7fab0ff57b87
PeachSQL <peachsql@gmail.com>
2025-11-26 15:19:31 +0300

    studying functions in python

---
+++ functions.py
@@ -0,0 +1,14 @@
+#A function to greet the user by name
+#def greeting(name):
+  #print(f"Welcome {name}. How are you doing today?")
+
+#greeting("Tevin")
+
+#A function to calculate the area of a rectangle
+def area_rectangle(length, width):
+    return length * width
+
+length = int(input("What is the length of the rectangle? "))
+width = int(input("What is the width of the rectangle? "))
+
+print(area_rectangle(length, width))
