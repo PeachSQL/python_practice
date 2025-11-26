b93b9dcac2eeb9288127d1364c0db845c459a8e9
PeachSQL <peachsql@gmail.com>
2025-11-26 15:29:19 +0300

    studying functions in python

--- functions.py
+++ functions.py
@@ -5,10 +5,22 @@
 #greeting("Tevin")
 
 #A function to calculate the area of a rectangle
-def area_rectangle(length, width):
-    return length * width
+#def area_rectangle(length, width):
+    #return length * width
 
-length = int(input("What is the length of the rectangle? "))
-width = int(input("What is the width of the rectangle? "))
+#length = int(input("What is the length of the rectangle? "))
+#width = int(input("What is the width of the rectangle? "))
 
-print(area_rectangle(length, width))
+#print(area_rectangle(length, width))
+
+#A function to check if a number is odd or even
+def odd_even(number):
+    if number % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+
+number = int(input("What number can we check for you today? "))
+result = odd_even(number)
+
+print(f"Your number is {result}")
