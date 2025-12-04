7672519d45242bd5910c6df590d21bb57208c8e7
PeachSQL <peachsql@gmail.com>
2025-12-04 08:50:04 +0300

    mathematical operators and data types in python

---
+++ helloworld.py
@@ -0,0 +1,22 @@
+#checking python operators
+print(3+2) #addition
+print(3-2) #subtraction
+print(3*2) #multiplication
+print(3**2) #exponential
+print(3%2) #modulus provides the remainder after a division operation
+print(3/2) #division
+print(3//2) #floor division
+
+#checking data types
+
+print(type(10)) #Integer - int
+print(type(10.50)) #float - decimal
+print(type(1 + 3j)) #complex
+print(type("Stella")) #string
+print(type('Stella')) #string
+print(type([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])) #list
+print(type({'name': 'Stella', 'Gender': 'Female'})) #dictionary
+print(type({1.25, 4.65, 9.04})) #set
+print(type((1.25, 4.65, 9.04))) #tuple
+print(type(3 == 3)) #bool
+print(type (3 >= 3)) #bool
